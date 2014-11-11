5f9d2964e39e19802e7b1f0d3cde0af5ea7c5f78
JotaFox <emiliano.tec92@gmail.com>
2014-11-10 23:18:27 -0200

    Ex3 em progresso

--- 02 - Lista 1 JAVA/Ex3/Ex3.java
+++ 02 - Lista 1 JAVA/Ex3/Ex3.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class Ex3{
 	public static void main(String[] args) {
 		
+		double[] valorDoAngulo; 
+
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Digite a medida do angulo: ");
-		double valorDoAngulo = sc.nextDouble();
+		System.out.println("Digite o numero de angulos:");
+		int n = sc.nextInt();
 
-		AnguloObj invocador = new AnguloObj(valorDoAngulo);
+        for (int i = 0; i < n; i++) {
 
-		invocador.funcaoSeno();
-		invocador.funcaoCoseno();
-		invocador.funcaoTangente();
-		invocador.funcaoCotangente();
-		System.out.println(invocador.toString());
+			System.out.println("Digite a medida do angulo: ");
+			valorDoAngulo[i] = sc.nextDouble();
+		}
 
+		// TO-DO MÉTODO DE IMPRESSÃO DO ARRAY DE STRINGS
 	}
 }
